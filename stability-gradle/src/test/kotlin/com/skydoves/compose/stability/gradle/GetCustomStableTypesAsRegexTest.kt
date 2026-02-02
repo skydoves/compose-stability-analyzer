ddba2e3b5419c1b9746f0084a297219cbe8368b4
Matej Drobnič <matej@matejdro.com>
2026-02-02 07:57:51 +0100

    Add a test for binary file

--- stability-gradle/src/test/kotlin/com/skydoves/compose/stability/gradle/GetCustomStableTypesAsRegexTest.kt
+++ stability-gradle/src/test/kotlin/com/skydoves/compose/stability/gradle/GetCustomStableTypesAsRegexTest.kt
@@ -1,6 +1,8 @@
 package com.skydoves.compose.stability.gradle
 
 import java.io.File
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 import kotlin.io.path.createTempDirectory
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
@@ -205,6 +207,30 @@ class GetCustomStableTypesAsRegexTest {
       result
     )
   }
+
+  @OptIn(ExperimentalEncodingApi::class)
+  @Test
+  fun `Do not crash when passed malformed files`() {
+    val file = File(targetFolder, "patterns.txt")
+    // Random string generated from the https://it-tools.tech/token-generator.
+    // It results in a random binary file
+    file.writeBytes(Base64.decode("SMeCMZ8rlwx1i42PskSpoYiF0fXmiYxWlHSk6yI0DifWgPTE7HWaHTz4dO2BP05lTYJdIPf8jcjGaI44UfBfWouZ42ogkafZvVRWq0RbQm74ScODlB4OWc5aXpKMecEbJduhlYSrOysYwvcAV0Yo9UOHjZ3U9H3uqsF182oXsK2HyUaZ0r7DDFVZej2VLpwFKEbgaMyP8Vwvx0szxRT3QIGXPJ5dtH3ZWJxRRPI5deID8hRjbZOHI1sF9Y7DHTPoTXUaphSaXSUACeBkUB7UMd9fUKwkx4pDa3Hujo2dksEqo5YYtOZATJH9oaDsMGDguCuTRLtbT9gmgFkTSMCHV35buQJDUKyQLPJcAxG4QDuGq9tcmvKGPr6wreqVSkAyyr7zl3SmSlHF"))
+
+    val classes = listOf(
+      "mypackage.ClassA",
+      "mypackage.ClassB",
+      "otherpackage.ClassA",
+      "otherpackage.ClassB",
+    )
+
+    val result = getMatches(listOf(file), classes)
+
+    assertEquals(
+      emptyList(),
+      result
+    )
+  }
+
 
   private fun getMatches(patterns: String, classes: List<String>): List<String> {
     val file = File(targetFolder, "patterns.txt")
